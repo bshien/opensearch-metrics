de0399c50004c3e0def044deb78a23c0506c4acb
Brandon Shien <bshien@amazon.com>
2024-06-21 09:26:59 -0700

    Changed alarm threshold from 100 to 50
    Signed-off-by: Brandon Shien <bshien@amazon.com>

--- infrastructure/lib/enums/project.ts
+++ infrastructure/lib/enums/project.ts
@@ -1,12 +1,12 @@
 enum Project{
-    AWS_ACCOUNT = '457182322296',
+    AWS_ACCOUNT = '',
     JENKINS_MASTER_ROLE = '',
     JENKINS_AGENT_ROLE = '',
-    REGION = 'us-east-1',
+    REGION = '',
     METRICS_HOSTED_ZONE = 'metrics.opensearch.org',
     METRICS_COGNITO_HOSTED_ZONE = '',
-    RESTRICTED_PREFIX = 'pl-60b85b09',
+    RESTRICTED_PREFIX = '',
     LAMBDA_PACKAGE = 'opensearch-metrics-1.0.zip',
-    EC2_AMI_SSM = '/aws/service/internal-ami-amazon-linux-latest/al2023-ami-naws-kernel-6.1-x86_64',
+    EC2_AMI_SSM = '',
 }
 export default Project;
